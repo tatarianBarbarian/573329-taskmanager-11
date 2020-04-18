c32819212cc1908315b9ada45fbfca7b3e745f4e
FK <asssama212@gmail.com>
2020-04-18 13:10:55 +0300

    Add time formatting for minutes

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,11 @@
+const castTimeFormat = (value) => {
+  return value < 10 ? `0${value}` : String(value);
+};
+
 export const formatTime = (date) => {
   const isItTwelveNow = date.getHours() % 12 === 0;
   const hours = isItTwelveNow ? date.getHours() : date.getHours() % 12;
-  const minutes = date.getMinutes();
+  const minutes = castTimeFormat(date.getMinutes());
   const timePeriodAbbreviation = date.getHours() > 12 ? `PM` : `AM`;
 
   return `${hours}:${minutes} ${timePeriodAbbreviation}`;
