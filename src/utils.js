988288d50e118664175bc20499690521e0f09409
FK <asssama212@gmail.com>
2020-04-18 12:32:36 +0300

    Fix time formatting

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,8 @@
-const castTimeFormat = (value) => {
-  return value < 10 ? `0${value}` : String(value);
-};
-
 export const formatTime = (date) => {
-  const hours = castTimeFormat(date.getHours() % 12);
-  const minutes = castTimeFormat(date.getMinutes());
+  const isItTwelveNow = date.getHours() % 12 === 0;
+  const hours = isItTwelveNow ? date.getHours() : date.getHours() % 12;
+  const minutes = date.getMinutes();
+  const timePeriodAbbreviation = date.getHours() > 12 ? `PM` : `AM`;
 
-  return `${hours}:${minutes}`;
+  return `${hours}:${minutes} ${timePeriodAbbreviation}`;
 };
